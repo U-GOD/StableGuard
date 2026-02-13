3d5e1c340b67ff7882f92c9314e550d828e7f3b3
UGOD <kabufredrick9@gmail.com>
2026-02-13 01:12:01 +0000

    Implement and test ZKVerifier stub

---
+++ contracts/test/ZKVerifier.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "forge-std/Test.sol";
+import "../src/ZKVerifier.sol";
+
+contract ZKVerifierTest is Test {
+    ZKVerifier public verifier;
+
+    event ProofVerified(bytes32 indexed proofHash, bool valid);
+
+    function setUp() public {
+        verifier = new ZKVerifier();
+    }
+
+    function test_VerifyProof_Stub() public {
+        uint256[2] memory a = [uint256(0), uint256(0)];
+        uint256[2][2] memory b = [
+            [uint256(0), uint256(0)],
+            [uint256(0), uint256(0)]
+        ];
+        uint256[2] memory c = [uint256(0), uint256(0)];
+        uint256[] memory input = new uint256[](0);
+
+        vm.expectEmit(false, false, false, true); // Don't check indexed hash for this test, just "valid"
+        emit ProofVerified(bytes32(0), true);
+
+        bool isValid = verifier.verifyProof(a, b, c, input);
+        assertTrue(isValid);
+    }
+}
